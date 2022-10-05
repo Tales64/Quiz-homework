9e126c3a6bfdcf1f8b4244e45b3ceabd42cc2ea0
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2022-10-04 23:49:32 -0700

    psuedo code

---
+++ 02-Challenge/Assets/sript.js
@@ -0,0 +1,22 @@
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// THEN I can save my initials and my score
+// 
+// 
+// 
+// 
+// create variables for all the questions in an array
+// create an array for all the potential answers 
+// create variables for all the correct answers
+// create a randomizer to choose questions to dissplay optional
+// create a for loop to continue choosing questions 
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Quiz-homework
 Homework #4 Quiz
+Make a quiz that starts, asked questions, records your score and holds that info in local storeage
